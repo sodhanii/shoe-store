1675fea4b7eb8dbfd6ba6118900bf5a6a446d361
Aditya Sodhani <aditya.sodhani@sodhanii.com>
2023-01-22 14:39:13 -0500

    Reactor code for sales record creation

--- store-backend/app/jobs/import_inventory_data_job.rb
+++ store-backend/app/jobs/import_inventory_data_job.rb
@@ -10,18 +10,7 @@ class ImportInventoryDataJob < ApplicationJob
     store = Store.find_or_create_by(name: store_name)
     model = Model.find_or_create_by(name: model_name)
 
-    inventory = StoreInventory.find_or_create_by(store_id: store.id, model_id: model.id)
-    inventory.update(:quantity => quantity)
-    if ( inventory.sale_quantity > 0 )
-      Sale.create(
-        :store_id => store.id,
-        :model_id => model.id,
-        :quantity => inventory.sale_quantity
-      )
-    end
-
-
-
+    StoreInventory.find_or_create_by(store_id: store.id, model_id: model.id).update(:quantity => quantity)
 
   end
 end
--- store-backend/app/models/store_inventory.rb
+++ store-backend/app/models/store_inventory.rb
@@ -1,9 +1,20 @@
 class StoreInventory < ApplicationRecord
   belongs_to :store
   belongs_to :model
+  after_update :create_sales_record
 
   def sale_quantity
     [0, quantity_before_last_save - quantity].max();
+  end
+
+  def create_sales_record
+    if ( sale_quantity > 0 )
+      Sale.create(
+        :store_id => store_id,
+        :model_id => model_id,
+        :quantity => sale_quantity
+      )
+    end
   end
 
 end
