8918d6fa7fde90dcc1da8950e2a9b863f495fcda
Aditya Sodhani <aditya.sodhani@sodhanii.com>
2023-01-23 11:42:25 -0500

    Redis fixes, integration

--- store-backend/config/environments/development.rb
+++ store-backend/config/environments/development.rb
@@ -19,19 +19,11 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
-    config.action_controller.perform_caching = true
-    config.action_controller.enable_fragment_cache_logging = true
+  config.action_controller.perform_caching = true
 
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
+  config.active_record.cache_versioning = false;
 
-    config.cache_store = :null_store
-  end
+  config.cache_store = :redis_store, { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0") }
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
@@ -65,6 +57,10 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
+  config.action_cable.url = "ws://localhost:3000/cable"
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, 
+  /https:\/\/*/]
+
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.disable_request_forgery_protection = true
 end
